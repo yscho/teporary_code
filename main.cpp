cc6c472e2199db92d9c0f402c6d3035db4e811be
youngsoo <moosikcom@gmail.com>
2022-01-17 21:54:55 +0900

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "solution.cpp"
+using namespace std;
+
+//static void printhello();
+
+int main(void)
+{
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	printhello();
+
+	return 0;
+}
